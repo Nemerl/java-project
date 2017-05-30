f554d1f0bf2917295bfb63787e276a6aa059c099
Marcin Krysta <blejlook@gmail.com>
2017-05-30 19:36:08 +0200

    Dodano klase Student

---
+++ src/main/java/pl/edu/pwsz/student/Student.java
@@ -0,0 +1,35 @@
+package pl.edu.pwsz.student;
+
+/**
+ * Created by Student on 2017-05-30.
+ */
+public class Student {
+
+    private int yearOfBrith;
+    private String name;
+    private String lastname;
+
+    public int getYearOfBrith() {
+        return yearOfBrith;
+    }
+
+    public void setYearOfBrith(int yearOfBrith) {
+        this.yearOfBrith = yearOfBrith;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
